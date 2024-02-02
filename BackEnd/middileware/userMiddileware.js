fec175dcc73e6fad060b506b944658125556a941
athuljain <athuljainkj2@gmail.com>
2024-02-02 11:36:08 +0530

    todo login and registraion front end and backend

--- BackEnd/middileware/userMiddileware.js
+++ BackEnd/middileware/userMiddileware.js
@@ -1,13 +1,11 @@
-
-
-//const schema=require("../model/userModel")
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const User = require('../model/userModel');
 
 const requireAuth = async (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    res.status(400).json({ error: "Authorization token required" });
+    return res.status(400).json({ error: "Authorization token required" });
   }
 
   const token = authorization.split(" ")[1];
@@ -15,8 +13,13 @@ const requireAuth = async (req, res, next) => {
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
 
-    req.user = await User.findOne({ _id }).select("_id");
+    const user = await User.findById(_id); // Find user by MongoDB _id
 
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    req.user = user; // Attach user to request object
     next();
   } catch (error) {
     res.status(400).json({ error: "Request is not authorized" });
--- Font-end/src/Component/TodoHome.jsx
+++ Font-end/src/Component/TodoHome.jsx
@@ -1,304 +1,70 @@
-// import React, { useState } from "react";
-// import '../Styles/TodoHome.css'
-// import {
-//   MDBBtn,
-//   MDBCard,
-//   MDBCardBody,
-//   MDBCheckbox,
-//   MDBCol,
-//   MDBContainer,
-//   MDBInput,
-//   MDBListGroup,
-//   MDBListGroupItem,
-//   MDBRow,
-//   MDBTabs,
-//   MDBTabsContent,
-//   MDBTabsItem,
-//   MDBTabsLink,
-//   MDBTabsPane,
-// } from "mdb-react-ui-kit";
-// import axios from 'axios'
-
-// export default function TodoHome() {
-//     const [active, setActive] = useState("tab1");
-//     const [taskText, setTaskText] = useState(''); // State to store the task text
-//     const [todoList, setTodoList] = useState([]); // State to store the todo list
-
-//   const handleClick = (value) => {
-//     if (value === active) {
-//       return;
-//     }
-
-//     setActive(value);
-//   };
-//   const handleAddTask = async () => {
-//     try {
-//       // Send a POST request to add the task
-//       const response = await axios.post('http://localhost:5000/user/addTodo', { userId: 'user_id_here', taskText });
-      
-//       // Update the todo list with the new task
-//       setTodoList([...todoList, { text: taskText, completed: false }]);
-      
-//       // Reset the task text input
-//       setTaskText('');
-      
-//       // Log the response from the server
-//       console.log(response.data);
-//     } catch (error) {
-//       console.error('Error adding task:', error);
-//     }
-//   };
-  
-
-//   return (
-//     <section className="gradient-custom vh-100">
-//       <MDBContainer className="py-5 h-100">
-//         <MDBRow className="d-flex justify-content-center align-items-center">
-//           <MDBCol xl="10">
-//             <MDBCard>
-//               <MDBCardBody className="p-5">
-//                 <div className="d-flex justify-content-center align-items-center mb-4">
-//                   <MDBInput
-//                     type="text"
-//                     id="form1"
-//                     label="New task..."
-//                     value={taskText}
-//                     onChange={(e)=>setTaskText(e.target.value)}
-//                     wrapperClass="flex-fill"
-//                   />
-//                   <MDBBtn type="submit" color="info" className="ms-2">
-//                     Add
-//                   </MDBBtn>
-//                 </div>
-//                 <MDBTabs className="mb-4 pb-2">
-//                   <MDBTabsItem>
-//                     <MDBTabsLink
-//                       onClick={() => handleClick("tab1")}
-//                       active={active === "tab1"}
-//                     >
-//                       All
-//                     </MDBTabsLink>
-//                   </MDBTabsItem>
-//                   <MDBTabsItem>
-//                     <MDBTabsLink
-//                       onClick={() => handleClick("tab2")}
-//                       active={active === "tab2"}
-//                     >
-//                       Active
-//                     </MDBTabsLink>
-//                   </MDBTabsItem>
-//                   <MDBTabsItem>
-//                     <MDBTabsLink
-//                       onClick={() => handleClick("tab3")}
-//                       active={active === "tab3"}
-//                     >
-//                       Completed
-//                     </MDBTabsLink>
-//                   </MDBTabsItem>
-//                 </MDBTabs>
-//                 <MDBTabsContent>
-//                   <MDBTabsPane show={active === "tab1"}>
-//                     <MDBListGroup className="mb-0">
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                           defaultChecked
-//                         />
-//                         <s>Cras justo odio</s>
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                           defaultChecked
-//                         />
-//                         <s>Dapibus ac facilisis in</s>
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Morbi leo risus
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Porta ac consectetur ac
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Vestibulum at eros
-//                       </MDBListGroupItem>
-//                     </MDBListGroup>
-//                   </MDBTabsPane>
-//                   <MDBTabsPane show={active === "tab2"}>
-//                     <MDBListGroup className="mb-0">
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Morbi leo risus
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Porta ac consectetur ac
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                         />
-//                         Vestibulum at eros
-//                       </MDBListGroupItem>
-//                     </MDBListGroup>
-//                   </MDBTabsPane>
-//                   <MDBTabsPane show={active === "tab3"}>
-//                     <MDBListGroup className="mb-0">
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                           defaultChecked
-//                         />
-//                         <s>Cras justo odio</s>
-//                       </MDBListGroupItem>
-//                       <MDBListGroupItem
-//                         className=" d-flex align-items-center border-0 mb-2 rounded"
-//                         style={{ backgroundColor: "#f4f6f7" }}
-//                       >
-//                         {" "}
-//                         <MDBCheckbox
-//                           name="flexCheck"
-//                           value=""
-//                           id="flexCheck"
-//                           className="me-3"
-//                           defaultChecked
-//                         />
-//                         <s>Dapibus ac facilisis in</s>
-//                       </MDBListGroupItem>
-//                     </MDBListGroup>
-//                   </MDBTabsPane>
-//                 </MDBTabsContent>
-//               </MDBCardBody>
-//             </MDBCard>
-//           </MDBCol>
-//         </MDBRow>
-//       </MDBContainer>
-//     </section>
-//   );
-// }
-
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const AddTodo = () => {
-  const [taskText, setTaskText] = useState('');
+export default function TodoHome() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [error, setError] = useState(null); // State to hold error information
 
-  const handleInputChange = (event) => {
-    setTaskText(event.target.value);
+  // Fetch all todos from the backend when the component mounts
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/todo'); // Updated endpoint
+      setTodos(response.data);
+      setError(null); // Clear any previous errors
+    } catch (error) {
+      console.error('Error fetching todos:', error);
+      setError('Failed to fetch todos. Please try again later.'); // Set error message
+    }
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    
+  const handleCreateTodo = async () => {
     try {
-      // Assuming userId is obtained from the authentication process
-      const userId = '';
-      
-      // Make a POST request to the server to add the todo
-      const response = await axios.post('http://localhost:5000/user/addTodo', { userId, taskText });
-      
-      // Handle success
-      console.log(response.data.message);
-      
-      // Reset input field
-      setTaskText('');
+      const response = await axios.post('http://localhost:8080/todo', { todo: newTodo }); // Updated endpoint
+      setTodos([...todos, response.data]);
+      setNewTodo('');
+      setError(null); // Clear any previous errors
     } catch (error) {
-      // Handle error
-      console.error('Error adding todo:', error.response.data.error);
+      console.error('Error creating todo:', error);
+      setError('Failed to create todo. Please try again later.'); // Set error message
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      await axios.delete(`http://localhost:8080/todo/${id}`); // Updated endpoint
+      setTodos(todos.filter(todo => todo._id !== id));
+      setError(null); // Clear any previous errors
+    } catch (error) {
+      console.error('Error deleting todo:', error);
+      setError('Failed to delete todo. Please try again later.'); // Set error message
     }
   };
 
   return (
     <div>
-      <h2>Add Todo</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Enter task"
-          value={taskText}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Add</button>
-      </form>
+      <h1>Todo List</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>} {/* Display error message */}
+      <input 
+        type="text" 
+        value={newTodo} 
+        onChange={(e) => setNewTodo(e.target.value)} 
+        placeholder="Enter todo..." 
+      />
+      <button onClick={handleCreateTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            {todo.todo}
+            <button onClick={() => handleDeleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
-
-export default AddTodo;
+}
