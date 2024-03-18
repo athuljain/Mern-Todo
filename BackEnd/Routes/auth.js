65563f2dff9c815e7edf09a7256937dfa56dbd31
athuljain <athuljainkj2@gmail.com>
2024-03-18 14:53:41 +0530

    todo List Auth setups

--- BackEnd/Routes/auth.js
+++ BackEnd/Routes/auth.js
@@ -1,28 +1,35 @@
 const router=require('express').Router();
 
-const user = require('../Models/user');
+// const user = require('../Models/user');
 const User=require("../Models/user")
+const bycrypt=require("bcrypt")
+
 
 //Sign in
+const router = require('express').Router();
+const User = require("../Models/user");
+const bcrypt = require("bcrypt");
 
-router.post('/register',async(req,res)=>{
-    try{
-        const {email,username,password}=req.body 
+// Sign in
+router.post('/register', async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const hashPassword = bcrypt.hashSync(password);
 
-        const user=new  User({email,username,password});
-        {email:email}
+        const user = new User({ email, username, password: hashPassword });
 
-        await user.save().then(()=>{
-            res.status(200).json({user:user})
-        })
-
-
-
-    }catch(error){
-
-        res.status(400).json({messsage:"user already exist"})
+        await user.save();
+        res.status(200).json({ user: user });
+    } catch (error) {
+        res.status(400).json({ message: "User already exists" });
     }
-})
+});
+
+module.exports = router;
+
+
+
+
 
 
 module.exports=router;
