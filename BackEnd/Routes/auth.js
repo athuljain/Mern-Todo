8b10f9f4388c57c58acc12518d219272501c5077
athuljain <athuljainkj2@gmail.com>
2024-03-19 09:06:26 +0530

    userRegistraionCompleted

--- BackEnd/Routes/auth.js
+++ BackEnd/Routes/auth.js
@@ -1,30 +1,22 @@
-const router=require('express').Router();
-
-// const user = require('../Models/user');
-const User=require("../Models/user")
-const bycrypt=require("bcrypt")
 
 
-// Sign in
+const router = require('express').Router();
+const User = require("../Models/user");
+const bcrypt = require("bcrypt");
+
+// Sign up
 router.post('/register', async (req, res) => {
     try {
         const { email, username, password } = req.body;
-        const hashPassword = bycrypt.hashSync(password);
+        const hashPassword = bcrypt.hashSync(password, 10); // Hash the password
 
-        const user = new User({ email, username, password: hashPassword });
+        const user = new User({ email, username, password: hashPassword }); // Store the hashed password
 
         await user.save();
-        res.status(200).json({ user: user });
+        res.status(200).json({ user });
     } catch (error) {
         res.status(400).json({ message: "User already exists" });
     }
 });
 
 module.exports = router;
-
-
-
-
-
-
-module.exports=router;
