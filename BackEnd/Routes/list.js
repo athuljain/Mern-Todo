6929ac9a655d27a680b2bf3406391046552212a0
athuljain <athuljainkj2@gmail.com>
2024-03-19 12:58:24 +0530

    Todo get task,updateTask,DeleteTask

--- BackEnd/Routes/list.js
+++ BackEnd/Routes/list.js
@@ -3,22 +3,8 @@ const router=require('express').Router();
 const User=require("../Models/user")
 const List=require("../Models/list")
 
-// router.post("/addtask", async(req,res)=>{
-// try {
-//     const {title,body,email}=req.body
-// const existingUser=await User.findOne({email})
-// if(existingUser){
-//     const list=new List({title,body,user:existingUser})
-//     await list.save().then(()=>res.status(200).json({list}))
-//     existingUser.list.push(list)
-//     existingUser.save()
-// }
-    
-// } catch (error) {
-//     console.log(error)
-// }
-// })
 
+//create taks
 
 router.post("/addtask", async (req, res) => {
     console.log("addtask");
@@ -46,6 +32,56 @@ router.post("/addtask", async (req, res) => {
         res.status(500).json({ message: "Internal server error." });
     }
 });
+
+
+//update:
+
+router.put("/updateTask/:id", async (req, res) => {
+   
+    try {
+        const { title, body, email } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        
+        if (existingUser) {
+          const list=  await List.findByIdAndUpdate(req.params.id,{title,body})
+            list.save().then(()=>res.status(200).json({message:"Task Updated"}))
+
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+});
+
+//Delete
+router.delete("/deleteTask/:id", async (req, res) => {
+    console.log("delete task");
+    try {
+        const {  email } = req.body;
+
+        const existingUser = await User.findOneAndUpdate({email},{$pull:{list:req.params.id}});
+        
+        if (existingUser) {
+          await List.findByIdAndDelete(req.params.id)
+            .then(()=>res.status(200).json({message:"Task Delete"}))
+
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+});
+
+
+//getTask
+
+router.get("/getTask/:id",async(req,res)=>{
+    const list=await List.find({user:req.params.id})
+    res.status(200).json({list})
+})
+
+
 
 
 module.exports=router
