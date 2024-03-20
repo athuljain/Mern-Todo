23cb56496952f2d71e3b381d25bddc9c0c538256
athuljain <athuljainkj2@gmail.com>
2024-03-20 12:37:13 +0530

    front-end just started

--- Font-end/src/App.js
+++ Font-end/src/App.js
@@ -1,38 +1,16 @@
 import React from 'react';
-import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Register from './Component/Register.jsx';
-import { myContext } from './Context.js';
-import { useState } from 'react';
-import Login from './Component/Login.jsx';
-import TodoHome from './Component/TodoHome.jsx';
 
 
 function App() {
 
 
-  const [user,setUser]=useState([])
 
 
-
-
-const values={user,setUser}
   return (
-    <MDBContainer fluid>
-      <div>
-        <BrowserRouter>
-  <myContext.Provider value={values}>
-    <Routes>
-       <Route path='/' element={<Login />}/>
-       <Route path='/register' element={<Register />}/>
-       <Route path='/todo' element={<TodoHome />} />
-     
-    </Routes>
-    </myContext.Provider>
-   </BrowserRouter>
-    </div>
-    </MDBContainer>
-  );
+<div>
+
+</div>
+  )
 }
 
 
